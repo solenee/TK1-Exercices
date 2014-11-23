0a83bc0f8411db9486389edba93a410c10608f5a
smndf <simon.dif.sd@gmail.com>
2014-11-23 09:15:48 +0100

    apparemment j'avais pas committé hier

--- TK1-Ex2ShoppingCart-Clients/src/client/Client_ImplSOAP.java
+++ TK1-Ex2ShoppingCart-Clients/src/client/Client_ImplSOAP.java
@@ -1,5 +1,21 @@
 package client;
 
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
 public class Client_ImplSOAP {
 
+	public static void main(String args[]) {
+		try {
+			Service service = Service.create(new URL("http://localhost:8090/services?wsdl"), new QName("http://tk.informatik.tu-darmstadt.de",
+					"Service_ImplSOAP")); 
+			//Service_ImplSOAP serviceSOAP = service.getPort(Service_ImplSOAP.class);
+			//System.out.println(shapeList.getVersion()); 
+			}
+		catch(Exception x) { x.printStackTrace();
+		}
+
+		}
 }
